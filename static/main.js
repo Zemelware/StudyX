88dfab876fb2c90aecedd12555e987240388275d
Ethan Zemelman <ethanzemelman@gmail.com>
2023-04-05 16:41:19 -0400

    Make code more concise

--- static/main.js
+++ static/main.js
@@ -1,7 +1,5 @@
-message_field = document.querySelector("textarea[name='message']");
-message_field.addEventListener("input", autoResize, false);
-
-function autoResize() {
-  this.style.height = "auto";
-  this.style.height = this.scrollHeight + "px";
-}
+messageField = document.querySelector("textarea[name='message']");
+messageField.addEventListener("input", () => {
+  // Automatically resize the message box
+  messageField.style.height = messageField.scrollHeight + "px";
+});
