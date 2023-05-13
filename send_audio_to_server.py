193aa03fa012d35d002c46bbc678b07ca2b3d1fb
Ethan Zemelman <ethanzemelman1@gmail.com>
2023-05-13 18:59:38 -0400

    Put code that sends request request in a function

--- send_audio_to_server.py
+++ send_audio_to_server.py
@@ -8,6 +8,8 @@ load_dotenv()
 url = "http://localhost:5001/upload-audio"
 api_key = os.getenv("CLIENT_API_KEY")
 
-with open("recording_og.wav", "rb") as f:
-    response = requests.post(url, headers={"api-key": api_key},
-                             files={"recording": f})
+
+def send_audio_to_server(file_path):
+    with open(file_path, "rb") as f:
+        response = requests.post(url, headers={"api-key": api_key},
+                                 files={"recording": f})
