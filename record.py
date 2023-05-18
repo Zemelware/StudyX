d5f7ba847b5ae9cb1c0977b42a163e910bdb2506
Ethan Zemelman <ethanzemelman@gmail.com>
2023-05-18 18:23:55 -0400

    Listen for button presses in a loop & boost audio volume

--- record.py
+++ record.py
@@ -1,22 +1,24 @@
+import os
 import wave
 from time import sleep
+from pydub import AudioSegment
 
 import pyaudio
 import RPi.GPIO as GPIO
 
-from send_audio_to_server import send_audio_to_server
+recording = False
+led_on = False
 
 LED_PIN = 19
 BUTTON_PIN = 16
 
-recording = False
-led_on = False
-
+# Setup the pins
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(LED_PIN, GPIO.OUT)
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+# Initialize some variables for pyaudio
 chunk = 4096
 sample_format = pyaudio.paInt16
 channels = 1
@@ -25,58 +27,59 @@ filename = "static/recording.wav"
 
 p = pyaudio.PyAudio()
 
+while True:
 
-stream = p.open(format=sample_format,
-                channels=channels,
-                rate=fs,
-                frames_per_buffer=chunk,
-                input=True)
+    frames = []  # Initialize array to store frames
 
-frames = []  # Initialize array to store frames
-
-while not recording:
-    # Check if the button was pressed
-    if GPIO.input(BUTTON_PIN) == GPIO.HIGH:
-        recording = True
-        sleep(0.5)
-        print("button on: " + str(recording))
+    while not recording:
+        # Check if the button was pressed
+        if GPIO.input(BUTTON_PIN) == GPIO.HIGH:
+            stream = p.open(format=sample_format,
+                            channels=channels,
+                            rate=fs,
+                            frames_per_buffer=chunk,
+                            input=True)
+            recording = True
+            sleep(0.5)
 
 
 # Recording loop
-while recording:
-    print("recording")
-    # Check if the LED is off. If so, turn it on.
-    if not led_on:
-        led_on = True
-        GPIO.output(LED_PIN, GPIO.HIGH)
-        print("LED on")
+    while recording:
+        data = stream.read(chunk, exception_on_overflow=False)
+        frames.append(data)
 
-    # Check if the button is pressed again. If so, turn the LED off and break out of the loop.
-    if GPIO.input(BUTTON_PIN) == GPIO.HIGH:
-        print("button was pressed while recording")
-        # Turn the LED off
-        recording = False
-        GPIO.output(LED_PIN, GPIO.LOW)
+        # Check if the LED is off. If so, turn it on.
+        if not led_on:
+            led_on = True
+            GPIO.output(LED_PIN, GPIO.HIGH)
 
-        led_on = False
-        break
+        # Check if the button is pressed again. If so, turn the LED off and break out of the loop.
+        if GPIO.input(BUTTON_PIN) == GPIO.HIGH:
+            # Turn the LED off
+            GPIO.output(LED_PIN, GPIO.LOW)
+            led_on = False
 
-    data = stream.read(chunk)
-    frames.append(data)
+            # Stop and close the stream
+            stream.stop_stream()
+            stream.close()
+            # Terminate the PortAudio interface
+            # p.terminate()
+            
+            # Save the recorded data as a WAV file
+            wf = wave.open(filename, 'wb')
+            wf.setnchannels(channels)
+            wf.setsampwidth(p.get_sample_size(sample_format))
+            wf.setframerate(fs)
+            wf.writeframes(b''.join(frames))
+            wf.close()
 
-# Stop and close the stream
-stream.stop_stream()
-stream.close()
-# Terminate the PortAudio interface
-p.terminate()
+            filename_mp3 = filename.split(".")[0] + "mp3"
+            # Boost the audio to make it louder
+            song = AudioSegment.from_wav(filename)
+            song = song + 35  # Number of DB to increase by
+            song.export(filename_mp3, format="mp3")
 
+            os.remove(filename)
 
-# Save the recorded data as a WAV file
-wf = wave.open(filename, 'wb')
-wf.setnchannels(channels)
-wf.setsampwidth(p.get_sample_size(sample_format))
-wf.setframerate(fs)
-wf.writeframes(b''.join(frames))
-wf.close()
+            recording = False
 
-# send_audio_to_server(filename)
